61fe59869f90552ea2064f0e25278fcdd68bd292
Ernesto Vargas <ernesto_vargas@yahoo.com>
2022-10-11 09:04:26 -0600

    Update script

---
+++ index.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+echo -e "\nCLEANING UP DEPLOY DIR..."
+rm -Rf .deploy/*
+
+echo -e "\n\nBUILDING CHARTS..."
+for chrt in api-creditcollection \
+            api-creditengine \
+            api-creditorigination \
+            api-creditstore \
+            api-notification \
+            api-notification-engine \
+            api-rules \
+            api-rules-engine \
+            api-security \
+            creditcollection \
+            creditflow \
+            creditstore \
+            customerservice \
+            origination-manager \
+            productmodeler \
+            rules-manager \
+            security-manager \
+            workflow-admin \
+            workflow-api-admin \
+            workflow-api-auth \
+            workflow-api-dashboard \
+            workflow-api-designer \
+            workflow-api-interaction \
+            workflow-base-web \
+            workflow-dashboard \
+            workflow-dashboard-simulator
+do
+  helm lint charts/$GITHUB_TOKEN
+  helm package charts/$chrt --destination .deploy
+done
+echo -e "\n\nUPLOADING CHARTS..."
+cr upload -o creditforce -r helm-chart -p .deploy --skip-existing -t GITHUB_TOKEN
+# cr index -i ./index.yaml -p .deploy --owner creditforce --git-repo helm-chart -t GITHUB_TOKEN
+# git add index.yaml
+# git commit -am"Update Helm chart repo index with new charts"
+# git push
+# echo -e "\n\nSLEEP 60..."
+# sleep 60
+# helm repo update
+echo -e "\n\nBUILDING PACKGES CHARTS..."
+for pckg in base \
+            collection \
+            origination \
+            portfolio \
+            rules \
+            store \
+            workflow
+do
+  helm dependency update charts/$pckg
+  helm package charts/$pckg --destination .deploy
+done
+echo -e "\n\nUPDATING HELM REPO..."
+cr upload -o creditforce -r helm-chart -p .deploy --skip-existing -t GITHUB_TOKEN
+cr index -i ./index.yaml -p .deploy --owner creditforce --git-repo helm-chart -t GITHUB_TOKEN
+git add index.yaml
+git commit -am"Update Helm chart repo index with new packages"
+git push
